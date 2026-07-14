ddb030d619844c6d7e230f612048a020d13b02d1
Faradilla Maulia <146446120+faradilla22@users.noreply.github.com>
2026-07-14 16:06:03 +0700

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,7 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
-import iconAsset from "../assets/icon-website.png.asset.json";
+import iconUrl from "../assets/icon-website.png";
 
 function NotFoundComponent() {
   return (
@@ -99,7 +99,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", type: "image/png", href: iconAsset.url },
+      { rel: "icon", type: "image/png", href: iconUrl },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       {
         rel: "preconnect",
